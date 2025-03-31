c3ab49847a5ceefa1f8a5dc7ffe0b011bcfb8c10
HotsixMan <hotsixman05@gmail.com>
2025-04-01 00:44:19 +0900

    .

--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -1,18 +1,1 @@
-<script lang="ts">
-    import PageTitle from "$lib/components/common/PageTitle.svelte";
-    import MainBanner from "$lib/components/page/(index)/MainBanner.svelte";
-    import MainNotice from "$lib/components/page/(index)/MainNotice.svelte";
-    import MainSearch from "$lib/components/page/(index)/MainSearch.svelte";
-    import MainShortcut from "$lib/components/page/(index)/MainShortcut.svelte";
-    import { Center } from "$lib/module/common/styled";
-
-    let {data} = $props();
-</script>
-
-<PageTitle />
-<Center>
-    <MainSearch />
-    <MainShortcut />
-    <MainBanner banners={data.mainBanners} />
-    <MainNotice notices={data.recentNotices} />
-</Center>
+고장
